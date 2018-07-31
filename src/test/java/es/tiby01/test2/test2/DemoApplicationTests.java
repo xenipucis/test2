1a13678515208bfa3931d4ed928ac620cd31876e
Adrian Tiberiu (contractor) <Adrian.Tiberiu@sita.aero>
2018-07-31 10:08:52 +0100

    Initial import

---
+++ src/test/java/es/tiby01/test2/test2/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package es.tiby01.test2.test2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
